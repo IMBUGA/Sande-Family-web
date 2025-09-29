0e8f833318dc4b6e561405e4e68059ca79754ef9
IMBUGA <danmarksande@gmail.com>
2025-09-29 18:54:12 +0300

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [
-        'https://your-web.vercel.app',
+        'https://sande-family-web.vercel.app/',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:5000'
